1e4a1776d65b04003b6dd94e3c75e11b777b0d3b
SONALI SONI <112364820+sonalisoni89d@users.noreply.github.com>
2025-10-28 11:45:28 +0530

    Update Auth1Application.java

--- auth1/src/main/java/com/auth1/Auth1Application.java
+++ auth1/src/main/java/com/auth1/Auth1Application.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Auth1Application {
-int new=568;
-String s="sonali soni";
+   int new=568;
+   String s="sonali soni";
+   String r="Ranjan Priye";
 	public static void main(String[] args) {
 		SpringApplication.run(Auth1Application.class, args);
 	}
