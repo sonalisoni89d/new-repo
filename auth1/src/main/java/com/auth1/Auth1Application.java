faa1c61d416d2bbc250673b890e9492af3444c03
SONALI SONI <112364820+sonalisoni89d@users.noreply.github.com>
2025-10-28 11:37:18 +0530

    Update Auth1Application.java

--- auth1/src/main/java/com/auth1/Auth1Application.java
+++ auth1/src/main/java/com/auth1/Auth1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Auth1Application {
-
+int new=568;
 	public static void main(String[] args) {
 		SpringApplication.run(Auth1Application.class, args);
 	}
